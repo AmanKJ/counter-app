064c455103d82d46ba09392b92c65474c871814f
Aman Kumar <mraman1997@gmail.com>
2020-03-27 18:42:09 +0530

    Complete App.
    Work done. Complete app is ready.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Counters from "./components/counters";
+import Navbar from "./components/navbar";
+
+class App extends Component {
+  state = {
+    counter: [
+      { id: 1, value: 0 },
+      { id: 2, value: 3 },
+      { id: 3, value: 0 },
+      { id: 4, value: 2 }
+    ]
+  };
+
+  handleReset = () => {
+    // const counters = [...this.state.counter];
+    // counters.forEach(count => (count.value = 0));
+    let counters = [];
+    if (this.state.counter.length > 0) {
+      counters = this.state.counter.map(c => {
+        c.value = 0;
+        return c;
+      });
+    } else {
+      counters = [
+        { id: 1, value: 0 },
+        { id: 2, value: 3 },
+        { id: 3, value: 0 },
+        { id: 4, value: 2 }
+      ];
+    }
+
+    this.setState({ counter: counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counter];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counter: counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counter.filter(c => c.id !== counterId);
+    this.setState({ counter: counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCounters={this.state.counter.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counter={this.state.counter}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/counter.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <span
+          style={{ fontSize: 20, color: "white" }}
+          className={this.getBadgeClasses()}
+        >
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        <br />
+        {/* <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul> */}
+      </React.Fragment>
+    ); // uses React.createElement thats why importing react
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
